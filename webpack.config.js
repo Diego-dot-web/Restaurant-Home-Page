5a2f45d5b5da77b5d8abd0416fef138d0b4822b2
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-01-22 14:13:21 -0500

    Fix Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,6 +13,9 @@ module.exports = {
     devServer: {
         static: './dist',
     },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
     module: {
         rules: [
             {
